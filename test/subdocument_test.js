c9165b39da865711ff3cb519dce20480365f34b3
puchk <kpuchalski@gmail.com>
2017-09-20 23:21:09 -0700

    adding and removing subdocuments from an existing record

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -15,4 +15,42 @@ describe('Subdocuments', () => {
 				done();
 			});
 	});
+
+	it('can add subdocuments to an existing record', (done) => {
+		const kevin = new User({
+			name: 'Kevin', 
+			posts: []
+		});
+
+		kevin.save()
+			.then(() => User.findOne({ name: 'Kevin' }))
+			.then((user) => {
+				user.posts.push({ title: 'New Post' });
+				return user.save();
+			})
+			.then(() => User.findOne({ name: 'Kevin' }))
+			.then((user) => {
+				assert(user.posts[0].title === 'New Post');
+				done();
+			});
+	});
+
+	it('can remove an existing subdocument', (done) => {
+		const kevin = new User({
+				name: 'Kevin', 
+				posts: [{ title: 'New title' }]
+		});
+		kevin.save()
+			.then(() => User.findOne({ name: 'Kevin' }))
+			.then((user) => {
+				const post = user.posts[0];
+				post.remove();
+				return user.save();
+			})
+			.then(() => User.findOne({ name: 'Kevin' }))
+			.then((user) => {
+				assert(user.posts.length === 0);
+				done();
+			});			
+	});
 });
